39d529101b6c7b47a383c33628549d2d663b7f14
Álvaro Miguel <alvarossmiguel@gmail.com>
2022-06-14 12:19:05 -0300

    fix: body parsing

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import { connect } from './database'
 import routes from './src/routes/api'
 
 const server = express()
 
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(routes)
 
 server.listen(8000, '', async () => {
