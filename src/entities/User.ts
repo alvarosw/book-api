cd3f96e047baf3eb7b72486e08ae93a40466af63
Álvaro Miguel <alvarossmiguel@gmail.com>
2022-06-14 14:28:13 -0300

    fix: specify table name in model

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({ synchronize: false })
+@Entity({ synchronize: false, name: 'users' })
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn("increment")
   id: number;
