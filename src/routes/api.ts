a83462f635a8f5dee002fa07aee9a7742e55f061
Álvaro Miguel <alvarossmiguel@gmail.com>
2022-06-14 14:26:30 -0300

    feat: login

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
+import { UserController } from '../controllers/index'
+const userController = new UserController()
 
 const api = Router()
 
-api.post('/login')
+api.post('/login', userController.login)
 
 export default api
