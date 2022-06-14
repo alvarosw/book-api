4b35e40d959014726e8e36b1de6aa40eba90b88e
Álvaro Miguel <alvarossmiguel@gmail.com>
2022-06-14 11:16:13 -0300

    refactor: js to ts

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import { connect } from './database'
 import routes from './src/routes/api'
 
 const server = express()
 
 server.use(routes)
 
-server.listen(8000)
+server.listen(8000, '', async () => {
+  await connect()
+})
