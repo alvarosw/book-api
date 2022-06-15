5b69e4560bbd66481013602c64562c6729f5c0c6
Álvaro Miguel <alvarossmiguel@gmail.com>
2022-06-15 17:47:30 -0300

    feat: set jest and add tests for users and books

---
+++ src/tests/mock/bookMock.ts
@@ -0,0 +1,22 @@
+const bookMock = {
+  validBook: {
+    titulo: 'A revoluçao dos bichos',
+    autor: 'George Orwell',
+    sinopse: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu varius diam. Aenean porttitor massa nibh, a facilisis mi eleifend in. Aenean aliquam bibendum nunc vitae scelerisque.'
+  },
+  validBook2: {
+    titulo: 'Javascript para iniciantes',
+    autor: 'Gustavo Guanabara',
+    sinopse: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu varius diam. Aenean porttitor massa nibh, a facilisis mi eleifend in. Aenean aliquam bibendum nunc vitae scelerisque.'
+  },
+  invalidBook: {
+    titulo: 'Livro incompleto',
+  },
+  bookTenant: {
+    nome: 'John Doe',
+    email: 'johndoe@email.com',
+    password: 'pass'
+  }
+}
+
+export default bookMock
